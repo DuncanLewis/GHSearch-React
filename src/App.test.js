2e4b765f8da354e5f3838ec78dbd63058d89825a
Duncan Lewis <duncan@piranhr.com>
2018-03-19 19:44:31 +0000

    Can render app without crashing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
+/* global it, expect */
+
+import { shallow } from 'enzyme';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('App renders without crashing', () => {
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
 });
